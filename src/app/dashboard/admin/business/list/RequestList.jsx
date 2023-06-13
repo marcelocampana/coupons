b6230e05997b2765a38b21adb56b0833485d293e
Marcelo Campaña <46510921+marcelocampana@users.noreply.github.com>
2023-06-13 13:04:21 -0400

    modal colors

--- src/app/dashboard/admin/business/list/RequestList.jsx
+++ src/app/dashboard/admin/business/list/RequestList.jsx
@@ -107,9 +107,9 @@ const RequestList = async () => {
                     <div
                       className={classNames(
                         "inline-flex items-center rounded-full px-2 py-1 text-xs font-medium  ring-1 ring-inset",
-                        request.request_statuss === "En revisión"
+                        request.request_status === "En revisión"
                           ? "bg-yellow-50 text-yellow-600 ring-yellow-600/20"
-                          : request.request_status
+                          : request.request_status === "Admitida"
                           ? "bg-green-50 text-green-600 ring-green-600/20"
                           : request.request_status
                           ? "bg-gray-50 text-gray-600 ring-gray-600/20"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  purge: {
+    enabled: true,
+    content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    options: {
+      safelist: [
+        "bg-red-600",
+        "text-red-600",
 
+        // Agrega aquí las clases que deseas mantener explícitamente
+      ],
+    },
+  },
   plugins: [
     require("@tailwindcss/forms"),
     require("@tailwindcss/aspect-ratio"),
